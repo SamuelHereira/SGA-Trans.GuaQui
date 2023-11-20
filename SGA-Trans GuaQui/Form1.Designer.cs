b47ac27c2ad4ac6a12443ef55a6ce9173b08600f
samuelhereirad <shereira@links.com.ec>
2023-11-19 22:10:53 -0500

    Diseño de pantalla de login

--- SGA-Trans GuaQui/Form1.Designer.cs
+++ SGA-Trans GuaQui/Form1.Designer.cs
@@ -44,7 +44,7 @@ namespace SGA_Trans_GuaQui
             this.button1.BackColor = System.Drawing.Color.RoyalBlue;
             this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(267, 318);
+            this.button1.Location = new System.Drawing.Point(267, 344);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(256, 38);
@@ -57,7 +57,7 @@ namespace SGA_Trans_GuaQui
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(357, 197);
+            this.label1.Location = new System.Drawing.Point(364, 215);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 20);
             this.label1.TabIndex = 1;
@@ -66,7 +66,7 @@ namespace SGA_Trans_GuaQui
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(267, 220);
+            this.textBox1.Location = new System.Drawing.Point(267, 238);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(252, 20);
             this.textBox1.TabIndex = 2;
@@ -74,7 +74,7 @@ namespace SGA_Trans_GuaQui
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(267, 275);
+            this.textBox2.Location = new System.Drawing.Point(267, 301);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(252, 20);
             this.textBox2.TabIndex = 4;
@@ -84,7 +84,7 @@ namespace SGA_Trans_GuaQui
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(344, 252);
+            this.label2.Location = new System.Drawing.Point(346, 278);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 20);
             this.label2.TabIndex = 3;
